cf6fb917dce73af85dd65135fd341beafe1508c2
TheCobraBoy2 <landoncs10@gmail.com>
2025-03-09 15:49:05 -0500

    Make alexandrite lamp respond to redstone signals
    Signed-off-by: TheCobraBoy2 <landoncs10@gmail.com>

--- src/main/java/net/vorrikz/firstmod/block/custom/AlexandriteLampBlock.java
+++ src/main/java/net/vorrikz/firstmod/block/custom/AlexandriteLampBlock.java
@@ -4,14 +4,14 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.phys.BlockHitResult;
-import org.jetbrains.annotations.Nullable;
 
 public class AlexandriteLampBlock extends Block {
     public static final BooleanProperty CLICKED = BooleanProperty.create("clicked");
@@ -34,5 +34,38 @@ public class AlexandriteLampBlock extends Block {
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
         pBuilder.add(CLICKED);
+    }
+
+    @Override
+    public void neighborChanged(BlockState state, Level level, BlockPos pos, Block block, BlockPos fromPos, boolean isMoving) {
+        super.neighborChanged(state, level, pos, block, fromPos, isMoving);
+
+        // Check if this block is providing a redstone signal
+        int power = level.getSignal(fromPos, Direction.UP); // Get signal power from target block
+
+        if (power > 0 && !state.getValue(CLICKED)) {
+            level.setBlockAndUpdate(pos, state.setValue(CLICKED, true)); // Turn on the lamp
+        } else if (power == 0 && state.getValue(CLICKED)) {
+            level.setBlockAndUpdate(pos, state.setValue(CLICKED, false)); // Turn off the lamp
+        }
+
+        // Check for redstone power from other blocks (redstone wire, torch, etc.)
+        power = level.getSignal(pos, Direction.UP); // Get the redstone signal at the position of this block
+        if (power > 0 && !state.getValue(CLICKED)) {
+            level.setBlockAndUpdate(pos, state.setValue(CLICKED, true)); // Turn on the lamp
+        } else if (power == 0 && state.getValue(CLICKED)) {
+            level.setBlockAndUpdate(pos, state.setValue(CLICKED, false)); // Turn off the lamp
+        }
+    }
+
+
+    @Override
+    protected boolean hasAnalogOutputSignal(BlockState pState) {
+        return pState.getValue(CLICKED);
+    }
+
+    @Override
+    protected int getAnalogOutputSignal(BlockState pState, Level pLevel, BlockPos pPos) {
+        return pState.getValue(CLICKED) ? 15: 0;
     }
 }
